62d7fa368cde7d55138f6067d677e168a1fd0ba2
angelapzpa <angelapalestina612@gmail.com>
2022-11-15 19:07:28 -0600

    api

---
+++ my_request.py
@@ -0,0 +1,19 @@
+import requests
+
+url = "https://rickandmortyapi.com/api/character"
+
+payload={}
+headers = {}
+
+response = requests.request("GET", url, headers=headers, data=payload)
+
+#print(response.json())
+respuesta_json = response.json()
+info = respuesta_json['info']
+personajes = respuesta_json['results']
+
+#for item in personajes:
+    #print(item['name'])
+
+for personaje in personajes:
+    print(f"The character {personaje['name']} is {personaje['status']} ")
